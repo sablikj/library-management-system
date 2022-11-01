53e210be2c0071aa2ef77ff7a682cd286973ea5e
Jan <j_sablik@utb.cz>
2022-11-01 17:24:03 +0100

    Added basic list of books to homepage

--- LibraryManagement/Controllers/HomeController.cs
+++ LibraryManagement/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using LibraryManagement.Models;
+using LibraryManagement.Models.Entity;
+using LibraryManagement.Services;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 using System.Diagnostics;
 using System.Security.Claims;
 
@@ -7,27 +10,40 @@ namespace LibraryManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> logger;
+        protected DatabaseService dbService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DatabaseService dbService)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.dbService = dbService;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(dbService.bookCollection.AsQueryable<Book>().ToList());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var filter = Builders<Book>.Filter.Eq(book => book.Id, id);
+            var book = await dbService.bookCollection.Find(filter).FirstOrDefaultAsync();
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
         }
     }
 }
